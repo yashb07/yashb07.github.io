fb44973171dc973c13f4ef6e84da822926e809b1
Yash <testemail@address.com>
2021-09-04 16:46:48 +0530

    Portfolio Website Initial Commit

--- aboutme.js
+++
@@ -1,8 +0,0 @@
-function aboutme() {
-  var x = document.getElementById("AboutMe");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
