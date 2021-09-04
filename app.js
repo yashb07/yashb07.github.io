5566e3d536e19318f3369909b73ff83d7866362b
Yash <testemail@address.com>
2021-09-04 23:05:24 +0530

    Final Commit - Fully Responsive
    Final Commit of Portfolio Website now fully responsive.

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 let mobileNav = document.querySelector('.mobile-nav');
 let hamburger = document.querySelector('.hamburger');
-let cancel = document.querySelector('.cancel');
+let cancel = document.querySelector('.cancel').onclick = function Function() {
+    mobileNav.classList.remove('open');
+};
+let home = document.querySelector('.homeBtn').onclick = function Function() {
+    mobileNav.classList.remove('open');
+};
+let about = document.querySelector('.aboutBtn').onclick = function Function() {
+    mobileNav.classList.remove('open');
+};
+let services = document.querySelector('.servicesBtn').onclick = function Function() {
+    mobileNav.classList.remove('open');
+};
+let projects = document.querySelector('.projectsBtn').onclick = function Function() {
+    mobileNav.classList.remove('open');
+};
+let contact = document.querySelector('.contactBtn').onclick = function Function() {
+    mobileNav.classList.remove('open');
+};
 
 hamburger.addEventListener('click', function() {
     mobileNav.classList.add('open');
 });
 
-cancel.addEventListener('click', function() {
-    mobileNav.classList.remove('open');
-});
+// cancel.addEventListener('click', function() {
+    
+// });
